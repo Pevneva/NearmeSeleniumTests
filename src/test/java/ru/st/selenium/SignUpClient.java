a088ec1497862ca8f74df88373eb5cc0144eb26f
Pevneva <lyudmila@realizeideas.net>
2013-07-08 19:40:19 +0300

    Added SignUpClient.java test

---
+++ src/test/java/ru/st/selenium/SignUpClient.java
@@ -0,0 +1,123 @@
+package ru.st.selenium;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.InputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+
+public class SignUpClient extends ru.st.selenium.pages.TestBase {
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  String respBody;
+  
+
+  @Test
+  public void testSignUpClient() throws Exception { 
+		
+		driver.manage().window().maximize();
+		
+		//Removing user with 'lyudmila_test_03@mail.ru' email
+		System.out.println("Removing user with 'lyudmila_test_03@mail.ru' email...");		
+		loginAsAdmin();
+		removeUser("lyudmila_test_03@mail.ru");
+		//Executing API Sign Up process
+		System.out.println("Executing API Sign Up process...");
+		String signupUri="http://95.110.204.46/nearme-portal/api/v1/signup?hash=3/3sVlGXuPb/IYUqbflui5DwjgIEJHAtCcYoJGrefVE=&email=lyudmila_test_03@mail.ru";
+		respBody=postRequest(signupUri);
+		System.out.println("Response:");
+		System.out.println(respBody);
+		
+		//Checking that Response Body is correct
+		System.out.println("Checking that Response Body is correct...");
+		checkResponse(respBody,"\"code\": 200");
+		checkResponse(respBody,"\"username\": \"TestUserName02\"");
+		checkResponse(respBody,"\"firstName\": \"__first_name__\"");
+		checkResponse(respBody,"\"lastName\": \"__last_name__\"");
+		checkResponse(respBody,"\"email\": \"lyudmila_test_03@mail.ru\"");
+		checkResponse(respBody,"\"accountType\": \"consumer\"");
+		checkResponse(respBody,"\"accountStatus\": \"active\"");
+		checkResponse(respBody,"\"birthday\": null");
+		checkResponse(respBody,"\"gender\": null");
+		checkResponse(respBody,"\"hometown\": null");
+		checkResponse(respBody,"\"photoUrl\": null");
+		System.out.println("OK");
+		
+		//Taking token for logout
+		System.out.println("Taking token...");
+		int num=indexOfToken(respBody,"\"token\"");
+		String token=respBody.substring(num+10, num+20);
+		int num2=token.indexOf("\"");
+		if (num2>-1) {			
+			token=token.substring(0,num2);
+			}
+		System.out.println("token="+token);
+		
+		//Executing API Log Out process with taken token
+		System.out.println("Executing API Log Out with taken token...");
+		String logoutUri="http://95.110.204.46/nearme-portal/api/v1/logout?hash=3/3sVlGXuPb/IYUqbflui5DwjgIEJHAtCcYoJGrefVE=";
+		respBody=postRequestWithToken(logoutUri,token);
+		System.out.println("Response:");
+		System.out.println(respBody);
+
+		System.out.println("Checking that Response Body is correct...");
+		checkResponse(respBody,"\"message\": \"User was successfully logged out\"");		
+		System.out.println("OK");
+		
+		//Executing API Log In process
+		System.out.println("Executing API Log In process...");
+		String loginUri="http://95.110.204.46/nearme-portal/api/v1/login?hash=3/3sVlGXuPb/IYUqbflui5DwjgIEJHAtCcYoJGrefVE=";
+		respBody=postRequest(loginUri);
+		System.out.println("Response:");
+		System.out.println(respBody);
+		checkResponse(respBody,"\"email\": \"lyudmila_test_03@mail.ru\"");
+
+		//Executing API Forgot process
+		System.out.println("Executing API Forgot process...");
+		String forgotUri="http://95.110.204.46/nearme-portal/api/v1/forgot?user=lyudmila_test_03@mail.ru";
+		respBody=postRequest(forgotUri);		
+		System.out.println("Response:");
+		System.out.println(respBody);
+		checkResponse(respBody,"\"message\": \"Password successfully changed and sent to User's email\"");
+	}
+   
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
--- src/test/java/ru/st/selenium/pages/TestBase.java
+++ src/test/java/ru/st/selenium/pages/TestBase.java
@@ -27,6 +27,16 @@ import ru.st.selenium.webdriver.WebDriverFactory;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import java.io.InputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 /*
  * Base class for all the test classes
  * 
@@ -283,6 +293,49 @@ public class TestBase {
     	Thread.sleep(1000);
     }
 }	
+
+public static String postRequest(String request_uri) throws Exception { 
+    DefaultHttpClient httpclient = new DefaultHttpClient();
+    HttpPost httpPost = new HttpPost(request_uri);		
+        try {
+		System.out.println("executing request:" );
+		System.out.println(httpPost.getURI());
+        // Create a response handler
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        String responseBody = httpclient.execute(httpPost, responseHandler);
+		return responseBody;
+	    } 
+		finally { httpPost.releaseConnection();}	
+}	
+
+public static String postRequestWithToken(String request_uri, String token) throws Exception { 
+    DefaultHttpClient httpclient = new DefaultHttpClient();
+	System.out.println("token inside = "+token);
+    HttpPost httpPost = new HttpPost(request_uri);		
+	httpPost.setHeader("session_token",token);	
+    try {
+		System.out.println(httpPost.getURI());
+        // Create a response handler
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        String responseBody = httpclient.execute(httpPost, responseHandler);
+		return responseBody;
+	    } 
+	finally { httpPost.releaseConnection();}	
+}
+
+  public static boolean	checkResponse(String str, String subStr) throws Exception {
+		boolean b;
+		int otv =str.indexOf(subStr);
+			if (otv>=0) {b=true;} 
+			else {
+			System.out.println("Response doesn't contain "+ subStr); 
+			b=false;
+			assertEquals(b,true);} 
+		return b;
+  }
+
+
+	
 		private boolean isElementPresent(By by) {
 		try {
 		driver.findElement(by);
